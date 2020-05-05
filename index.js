a8c133a9218c2bc167306dcdbdb056587a07f10d
Mateusz <matyd356@gmail.com>
2020-05-05 14:36:14 +0200

    allow adding playres to game

--- index.js
+++ index.js
@@ -28,22 +28,72 @@ const GameBoard = (() => {
             container.appendChild(div);
         };
     }
-    return { makeMove, renderBoard };
+    const addEventListeners = () => {
+        //showing forms
+        const forms = Array.from(document.querySelectorAll(".form"));
+        document.querySelector(".start").addEventListener("click", () => {
+            for (let i = 0; i < forms.length; i++) {
+                forms[i].style.display = "block";
+            }
+            gameFlow.changeGameState("creatingPlayers")
+        });
+        //create btns
+        const createButtons = Array.from(document.querySelectorAll(".create"));
+        for (let i = 0; i < createButtons.length; i++) {
+            if (createButtons[i].textContent == "Play vs Human") {
+                createButtons[i].addEventListener("click", () => {
+                    forms[1].style.display = "none";
+                    document.querySelector(".form-player-O").style.display = "block";
+                });
+            } else if (createButtons[i].textContent == "Create Player") {
+                let inputs = Array.from(document.querySelectorAll(".name-input"));
+                let playerName = null;
+                if (i == 0) {
+                    playerName = inputs[0].value
+                } else if (i == 3) {
+                    playerName = inputs[1].value
+                }
+                createButtons[i].addEventListener("click", () => {
+                    gameFlow.createPlayer(playerName, createButtons[i].id)
+                });
+            } else if (createButtons[i].textContent == "Play vs AI") {
+                createButtons[i].addEventListener("click", () => {
+                    gameFlow.createPlayer("AI", "O")
+                })
+            };
+        }
+    };
+    return { makeMove, renderBoard, addEventListeners };
 })();
 
 const Player = ((name, sign) => {
-    const getName = () => console.log(name);
+    const getName = () => name;
     const getSign = () => sign;
     return { getName, getSign };
 });
 
-const playerX = Player("jeff", "X");
-const playerY = Player("bob", "O");
-document.querySelector(".start").addEventListener("click", function () {
-    let forms = Array.from(document.querySelectorAll(".form"));
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].style.display = "block";
+const gameFlow = (() => {
+    let gameState = "start";
+    let playerX = null;
+    let playerO = null;
+
+    const changeGameState = (newState) => {
+        gameState = newState;
     }
-});
-GameBoard.makeMove(playerX.getSign(), 3)
-GameBoard.renderBoard();
+
+    const startGame = () => {
+        GameBoard.renderBoard();
+        GameBoard.addEventListeners();
+
+    }
+
+    const createPlayer = (name, sign) => {
+        if (sign == "X") { playerX = Player(name, sign) }
+        else if (sign == "O") { playerO = Player(name, sign) }
+        console.log(playerX.getName(), playerX.getSign())
+        console.log(playerO.getName(), playerO.getSign())
+    }
+    return { startGame, createPlayer, changeGameState }
+})();
+
+gameFlow.startGame()
